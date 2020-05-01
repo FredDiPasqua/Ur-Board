bef25f615321b0dbab3856287a8df72b278485ab
FredDiPassca <freddipassca@gmail.com>
2020-05-01 02:00:55 -0500

    no funciona el touch

--- test3.js
+++ test3.js
@@ -62,13 +62,13 @@ document.getElementById("paintbtn")
 
 var desactivar = (evento3) => {click = false};
 
-// cuadro.addEventListener("mousedown", activar);
-// cuadro.addEventListener("mouseup", desactivar);
-// cuadro.addEventListener("mousemove", pintar);
+cuadro.addEventListener("mousedown", activar);
+cuadro.addEventListener("mouseup", desactivar);
+cuadro.addEventListener("mousemove", pintar);
 
-cuadro.addEventListener("touchstart", activar);
-cuadro.addEventListener("touchend", desactivar);
-cuadro.addEventListener("touchmove", pintar);
+// cuadro.addEventListener("touchstart", activar);
+// cuadro.addEventListener("touchend", desactivar);
+// cuadro.addEventListener("touchmove", pintar);
 
 
 function dibujarLinea(color, xi, yi, xf, yf, lienzo) {
